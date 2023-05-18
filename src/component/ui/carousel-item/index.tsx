241148bcba3fff55bf4654e50a57135a2aff51cc
mistryaksh <mistryaksh1998@gmail.com>
2023-05-18 20:08:15 +0530

    update ui

--- src/component/ui/carousel-item/index.tsx
+++ src/component/ui/carousel-item/index.tsx
@@ -7,7 +7,7 @@ export interface CarouselItemProps {
 
 export const CarouselItem: FC<CarouselItemProps> = ({ altText, image }) => {
      return (
-          <div className="item xl:h-[60vh] lg:h-[60-vh]">
+          <div className="item xl:h-[100vh] lg:h-[100-vh]">
                <img src={image} width="100%" height="100%" alt={altText} className="object-cover" />
           </div>
      );
